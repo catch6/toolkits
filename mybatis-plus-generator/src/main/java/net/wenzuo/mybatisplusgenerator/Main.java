70b50c7a4dc3e4674cad6f0af8037e6f4b9a7864
Catch <catchlife6@163.com>
2022-11-07 15:18:17 +0800

    初始化

---
+++ mybatis-plus-generator/src/main/java/net/wenzuo/mybatisplusgenerator/Main.java
@@ -0,0 +1,12 @@
+package net.wenzuo.mybatisplusgenerator;
+
+/**
+ * @author Catch
+ * @since 2022-11-07
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
